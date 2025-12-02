b977ccf889aa496ef3e239abba2a5201d7f5245a
sergioperezcheco <104541981+sergioperezcheco@users.noreply.github.com>
2025-12-02 17:09:51 +0000

    Deploying to blog-pages from @ sergioperezcheco/vuepress-blog@ad89ef05b870c50b30c756993693d7d6c4670ab1 🚀

---
+++ assets/index.html-BDfmrhBz.js
@@ -0,0 +1,17 @@
+import{_ as e,c as t,a as o,o as n}from"./app-CaZ0cXcA.js";const c={};function s(r,h){return n(),t("div",null,[o(`
+这是一个博客主页的案例。
+
+要使用此布局，你应该在页面前端设置 \`layout: BlogHome\` 和 \`home: true\`。
+
+相关配置文档请见 [博客主页](https://theme-hope.vuejs.press/zh/guide/blog/home.html)。
+
+  - icon: user-group
+    name: 伙伴名称
+    desc: 伙伴详细介绍
+    link: https://你的伙伴链接
+
+  - icon: https://theme-hope-assets.vuejs.press/logo.svg
+    name: 自定义项目
+    desc: 自定义详细介绍
+    link: https://你的自定义链接
+`)])}const a=e(c,[["render",s],["__file","index.html.vue"]]),m=JSON.parse('{"path":"/","title":"博客主页","lang":"zh-CN","frontmatter":{"home":true,"layout":"BlogHome","icon":"house","title":"博客主页","heroImage":"https://picgo.checo.cc/20241106213358.png","heroText":"Checo的博客","heroFullScreen":true,"bgImage":"https://picgo.checo.cc/DSC_2017-3.jpg","tagline":"分享技术，记录生活","projects":[{"icon":"house","name":"个人主页","desc":"个人主页","link":"https://home.checo.cc"},{"icon":"bed","name":"免费图床","desc":"基于Telegram的开源图床","link":"https://img.checo.cc"},{"icon":"star","name":"星标文章","desc":"博客星标文章","link":"https://blog.checo.cc/star/"},{"icon":"image","name":"随机图片","desc":"生成随机图片URL","link":"https://random.checo.cc"}],"footer":"© 2025 Checo\'s Blog. All rights reserved.","head":[["meta",{"property":"og:url","content":"https://mister-hope.github.io/"}],["meta",{"property":"og:site_name","content":"Checo的博客"}],["meta",{"property":"og:title","content":"博客主页"}],["meta",{"property":"og:type","content":"website"}],["meta",{"property":"og:locale","content":"zh-CN"}],["meta",{"property":"og:updated_time","content":"2025-12-02T17:09:45.000Z"}],["meta",{"property":"article:modified_time","content":"2025-12-02T17:09:45.000Z"}],["script",{"type":"application/ld+json"},"{\\"@context\\":\\"https://schema.org\\",\\"@type\\":\\"WebPage\\",\\"name\\":\\"博客主页\\"}"]]},"headers":[],"git":{"createdTime":1738115513000,"updatedTime":1764695385000,"contributors":[{"name":"sergioperezcheco","username":"sergioperezcheco","email":"checo520@outlook.com","commits":9,"url":"https://github.com/sergioperezcheco"}]},"readingTime":{"minutes":0.71,"words":212},"filePathRelative":"README.md","localizedDate":"2025年1月29日","excerpt":"<!--\\n这是一个博客主页的案例。\\n\\n要使用此布局，你应该在页面前端设置 `layout: BlogHome` 和 `home: true`。\\n\\n相关配置文档请见 [博客主页](https://theme-hope.vuejs.press/zh/guide/blog/home.html)。\\n\\n  - icon: user-group\\n    name: 伙伴名称\\n    desc: 伙伴详细介绍\\n    link: https://你的伙伴链接\\n\\n  - icon: https://theme-hope-assets.vuejs.press/logo.svg\\n    name: 自定义项目\\n    desc: 自定义详细介绍\\n    link: https://你的自定义链接\\n-->"}');export{a as comp,m as data};
