749722ef73ddc3a253d62222933321251385bc03
sergioperezcheco <104541981+sergioperezcheco@users.noreply.github.com>
2026-06-23 12:58:55 +0000

    Deploying to blog-pages from @ sergioperezcheco/vuepress-blog@5b436f51b97d2d939d4c5efbaf335226371d8dda 🚀

---
+++ assets/README-DsOU6CU6.js
@@ -0,0 +1,17 @@
+import{D as e,E as t,F as n,t as r}from"./app-m_HMndrN.js";var i=JSON.parse('{"path":"/","title":"博客主页","lang":"zh-CN","frontmatter":{"home":true,"layout":"Blog","icon":"house","title":"博客主页","heroImage":"/logo.png","heroText":"Checo的博客","heroFullScreen":true,"bgImage":"https://picgo.checo.cc/DSC_2017-3.jpg","tagline":"分享技术，记录生活","projects":[{"icon":"house","name":"个人主页","desc":"关于我和常用链接","link":"https://home.checo.cc"},{"icon":"bed","name":"免费图床","desc":"基于 Telegram 的开源图床","link":"https://img.checo.cc"},{"icon":"star","name":"星标文章","desc":"值得优先阅读的技术记录","link":"https://blog.checo.cc/star/"},{"icon":"image","name":"随机图片","desc":"生成随机图片 URL","link":"https://random.checo.cc"}],"footer":"© 2026 Checo\'s Blog. All rights reserved.","head":[["script",{"type":"application/ld+json"},"{\\"@context\\":\\"https://schema.org\\",\\"@type\\":\\"WebPage\\",\\"name\\":\\"博客主页\\"}"],["meta",{"property":"og:url","content":"https://blog.checo.cc/"}],["meta",{"property":"og:site_name","content":"Checo的博客"}],["meta",{"property":"og:title","content":"博客主页"}],["meta",{"property":"og:type","content":"website"}],["meta",{"property":"og:locale","content":"zh-CN"}],["meta",{"property":"og:locale:alternate","content":"en-US"}],["meta",{"property":"og:updated_time","content":"2026-06-11T09:52:31.000Z"}],["meta",{"property":"article:modified_time","content":"2026-06-11T09:52:31.000Z"}],["link",{"rel":"alternate","hreflang":"en-us","href":"https://blog.checo.cc/en/"}],["link",{"rel":"alternate","type":"application/atom+xml","href":"https://blog.checo.cc/atom.xml","title":"Checo的博客 Atom Feed"}],["link",{"rel":"alternate","type":"application/json","href":"https://blog.checo.cc/feed.json","title":"Checo的博客 JSON Feed"}],["link",{"rel":"alternate","type":"application/rss+xml","href":"https://blog.checo.cc/rss.xml","title":"Checo的博客 RSS Feed"}]]},"git":{"createdTime":1738115513000,"updatedTime":1781171551000,"contributors":[{"name":"sergioperezcheco","username":"sergioperezcheco","email":"checo520@outlook.com","commits":11,"url":"https://github.com/sergioperezcheco"}]},"readingTime":{"minutes":0.73,"words":220},"filePathRelative":"README.md","excerpt":"<!--\\n这是一个博客主页的案例。\\n\\n要使用此布局，你应该在页面前端设置 `layout: Blog` 和 `home: true`。\\n\\n相关配置文档请见 [博客主页](https://theme-hope.vuejs.press/zh/guide/blog/home.html)。\\n\\n  - icon: user-group\\n    name: 伙伴名称\\n    desc: 伙伴详细介绍\\n    link: https://你的伙伴链接\\n\\n  - icon: https://theme-hope-assets.vuejs.press/logo.svg\\n    name: 自定义项目\\n    desc: 自定义详细介绍\\n    link: https://你的自定义链接\\n-->"}'),a={name:`README.md`};function o(r,i,a,o,s,c){return n(),e(`div`,null,[t(`
+这是一个博客主页的案例。
+
+要使用此布局，你应该在页面前端设置 \`layout: Blog\` 和 \`home: true\`。
+
+相关配置文档请见 [博客主页](https://theme-hope.vuejs.press/zh/guide/blog/home.html)。
+
+  - icon: user-group
+    name: 伙伴名称
+    desc: 伙伴详细介绍
+    link: https://你的伙伴链接
+
+  - icon: https://theme-hope-assets.vuejs.press/logo.svg
+    name: 自定义项目
+    desc: 自定义详细介绍
+    link: https://你的自定义链接
+`)])}var s=r(a,[[`render`,o]]);export{i as _pageData,s as default};
